dc47c5f630dedbeb75bc3d3374aefaddd4710004
Luke-l2e <overcharge2001@gmail.com>
2024-10-04 19:38:08 +0200

    Upgrade gradle version 8.7 -> 8.10.2 Add Scaffold Layout Add Top App Bar Add floating button Add first animation

--- app/src/main/java/de/hhn/randomgameselector/MainActivity.kt
+++ app/src/main/java/de/hhn/randomgameselector/MainActivity.kt
@@ -4,26 +4,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.hhn.randomgameselector.ui.theme.RandomGameSelectorTheme
 
+private const val s = "Jane's Federation of Nerds"
+
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RandomGameSelectorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var visible = remember {
+                    mutableStateOf(false)
+                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("Random Game Selector")
+                            }
+                        )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { visible.value = !visible.value }) {
+                            Icon(Icons.Default.Refresh, contentDescription = "Roll")
+                        }
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                    ) {
+
+                        AnimatedVisibility(visible.value) {
+                                TextField(
+                                    value = "Test",
+                                    onValueChange = {},
+                                    modifier = Modifier.animateEnterExit(
+                                        enter = slideInVertically()
+                                    )
+                                )
+                                TextField(value = "Def", onValueChange = {})
+                                TextField(value = "Abc", onValueChange = {})
+                        }
+                    }
                 }
             }
         }
